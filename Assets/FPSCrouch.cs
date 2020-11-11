e132192996e9db1285050e2571436eb330de0868
Jeeth <jeethsuresh@gmail.com>
2020-11-11 01:22:01 -0500

    add fps movement controls and a sample scene

---
+++ Assets/FPSCrouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCrouch : MonoBehaviour
+{
+    [SerializeField] float crouchHeight;
+    [SerializeField] float lyingHeight;
+    [SerializeField] float normalHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newScale = new Vector3(transform.localScale.x, normalHeight, transform.localScale.z);
+        if (Input.GetKey(KeyCode.C))
+        {
+            newScale.y = crouchHeight;
+        }
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            newScale.y = lyingHeight;
+        }
+        transform.localScale = newScale;
+    }
+}
---
+++ Assets/FPSMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed;
+    [SerializeField] float sprintMultiplier = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveBy = transform.right * x + transform.forward * y;
+        float actualSpeed = speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            actualSpeed *= sprintMultiplier;
+        }
+        rb.MovePosition(transform.position + moveBy.normalized * actualSpeed * Time.deltaTime);
+    }
+}
